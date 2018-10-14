44a7c32ffa62c40991d730460f31fe7f24be3981
dwaynedevelopment <dwaynedevelopment@gmail.com>
2018-10-14 16:10:52 -0400

    CREATED - Reorganized intial package directory setup.

--- app/src/main/java/com/dwaynedevelopment/passtimes/onboarding/activities/OnboardActivity.java
+++ app/src/main/java/com/dwaynedevelopment/passtimes/onboarding/activities/OnboardActivity.java
@@ -1,7 +1,9 @@
-package com.dwaynedevelopment.passtimes;
+package com.dwaynedevelopment.passtimes.onboarding.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.dwaynedevelopment.passtimes.R;
 
 public class OnboardActivity extends AppCompatActivity {
 
