d73d03262146fec33417a4c71375c6b4369828a9
dwaynedevelopment <dwaynedevelopment@gmail.com>
2018-10-14 15:43:29 -0400

    CREATED - Project inital stage.

---
+++ app/src/main/java/com/dwaynedevelopment/passtimes/OnboardActivity.java
@@ -0,0 +1,13 @@
+package com.dwaynedevelopment.passtimes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OnboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboard);
+    }
+}
