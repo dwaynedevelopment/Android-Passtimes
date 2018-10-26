ccf21d97a84ca7ce89b40222ab92a220bdac5f0b
dwaynedevelopment <dwaynedevelopment@gmail.com>
2018-10-25 22:27:35 -0400

    ISSUES - Need to loop through sizes to avoid pulling more data then needed.

--- app/src/main/java/com/dwaynedevelopment/passtimes/navigation/fragments/feed/FeedFragment.java
+++ app/src/main/java/com/dwaynedevelopment/passtimes/navigation/fragments/feed/FeedFragment.java
@@ -10,8 +10,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -23,59 +21,52 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
+import android.widget.ProgressBar;
 
 import com.dwaynedevelopment.passtimes.R;
 import com.dwaynedevelopment.passtimes.adapters.EventFeedViewAdapter;
-import com.dwaynedevelopment.passtimes.adapters.FavoriteViewAdapter;
-import com.dwaynedevelopment.passtimes.adapters.FeedOnGoingViewAdapter;
 import com.dwaynedevelopment.passtimes.models.Event;
-import com.dwaynedevelopment.passtimes.models.Player;
 import com.dwaynedevelopment.passtimes.models.Sport;
 import com.dwaynedevelopment.passtimes.navigation.fragments.event.CreateEventDialogFragment;
 import com.dwaynedevelopment.passtimes.navigation.fragments.event.ViewEventDialogFragment;
 import com.dwaynedevelopment.passtimes.utils.AuthUtils;
 import com.dwaynedevelopment.passtimes.utils.FirebaseFirestoreUtils;
-import com.firebase.ui.firestore.FirestoreRecyclerOptions;
-import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.Tasks;
-import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
-import org.w3c.dom.Document;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 import static com.dwaynedevelopment.passtimes.utils.KeyUtils.ACTION_EVENT_SELECTED;
 import static com.dwaynedevelopment.passtimes.utils.KeyUtils.DATABASE_REFERENCE_EVENTS;
-import static com.dwaynedevelopment.passtimes.utils.KeyUtils.DATABASE_REFERENCE_SPORTS;
 import static com.dwaynedevelopment.passtimes.utils.KeyUtils.DATABASE_REFERENCE_USERS;
 
 public class FeedFragment extends Fragment {
 
-    //private FeedOnGoingViewAdapter mAdapter;
     private FirebaseFirestoreUtils mDb;
     private RecyclerView eventsRecyclerView;
     private EventFeedViewAdapter eventFeedViewAdapter;
+
     private EventReceiver eventReceiver;
     private PopupMenu popupMenu;
+    private ProgressBar progressBar;
+
     private List<Sport> menuSports = new ArrayList<>();
     private Map<String, Event> mainFeedEvents = new HashMap<>();
+    private Map<String, Event> filteredEventsByCategory = new HashMap<>();
+    private List<String> selectedSports = new ArrayList<>();
+
+    private static final String TAG = "FeedFragment";
 
     public FeedFragment() {
     }
@@ -102,6 +93,9 @@ public class FeedFragment extends Fragment {
             feedToolbar.inflateMenu(R.menu.menu_feed);
             feedToolbar.setOnMenuItemClickListener(menuItemClickListener);
 
+            progressBar = view.findViewById(R.id.pb_feed);
+            progressBar.setVisibility(View.VISIBLE);
+
             eventReceiver = new EventReceiver();
             IntentFilter actionFilter = new IntentFilter();
             actionFilter.addAction(ACTION_EVENT_SELECTED);
@@ -114,63 +108,57 @@ public class FeedFragment extends Fragment {
             popupMenu.getMenu().add("All");
             popupMenu.getMenu().getItem(0).setChecked(true);
 
-            if (popupMenu.getMenu().getItem(0).isChecked()) {
-
-                //filteredEventsByCategory = mDb.filterEventByFavoriteSport(mainFeedEvents, selectedSports.get(0), selectedSports.get(1), selectedSports.get(2));
-                eventFeedViewAdapter = new EventFeedViewAdapter(mainFeedEvents, getActivity().getApplicationContext());
-
-                eventsRecyclerView = getView().findViewById(R.id.rv_ongoing);
-                eventsRecyclerView.setHasFixedSize(true);
-                eventsRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
-
-                eventsRecyclerView.setAdapter(eventFeedViewAdapter);
-            }
-
             mDb.databaseDocument(DATABASE_REFERENCE_USERS, mAuth.getCurrentSignedUser().getId())
                     .get().addOnCompleteListener(playerCompleteListener);
 
-            mDb.databaseCollection(DATABASE_REFERENCE_EVENTS).addSnapshotListener(new EventListener<QuerySnapshot>() {
-                @Override
-                public void onEvent(@javax.annotation.Nullable QuerySnapshot queryDocumentSnapshots,
-                                    @javax.annotation.Nullable FirebaseFirestoreException e) {
+            mDb.databaseCollection(DATABASE_REFERENCE_EVENTS)
+                    .addSnapshotListener(eventSnapshotListener);
 
-                    if (e != null) {
-                        Log.i(TAG, "onEvent: " + e.getLocalizedMessage());
-                        return;
-                    }
-                    if (queryDocumentSnapshots != null) {
-                        for (DocumentChange documentChange : queryDocumentSnapshots.getDocumentChanges()) {
+            setupInitalRecyclerView(true);
 
-                            if (documentChange != null) {
-                                switch (documentChange.getType()) {
-                                    case ADDED:
-                                        Event addedEvent = documentChange.getDocument().toObject(Event.class);
-                                        if (!mainFeedEvents.containsKey(addedEvent.getId())) {
-                                            mainFeedEvents.put(addedEvent.getId(), addedEvent);
-                                            //filteredEventsByCategory = mDb.filterEventByFavoriteSport(mainFeedEvents, intialFilter.get(0).getCategory(), intialFilter.get(1).getCategory(), intialFilter.get(2).getCategory());
-                                            Log.i(TAG, "onEvent: ADDED " + documentChange.getDocument().toObject(Event.class).toString());
 
-                                        } else {
-                                            Log.i(TAG, "onEvent: NOT ADDED " + documentChange.getDocument().toObject(Event.class).toString());
-                                        }
-                                        eventFeedViewAdapter.notifyDataSetChanged();
-                                        break;
-                                    case MODIFIED:
-                                        Log.i(TAG, "onEvent: MODIFIED " + documentChange.getDocument().toObject(Event.class).toString());
-                                        break;
-                                    case REMOVED:
-                                        Log.i(TAG, "onEvent: REMOVED " + documentChange.getDocument().toObject(Event.class).toString());
-                                        break;
-
-                                }
-                            }
-                        }
-                    }
-
-                }
-            });
         }
     }
+
+    private final EventListener<QuerySnapshot> eventSnapshotListener = new EventListener<QuerySnapshot>() {
+        @Override
+        public void onEvent(@javax.annotation.Nullable QuerySnapshot queryDocumentSnapshots,
+                            @javax.annotation.Nullable FirebaseFirestoreException e) {
+
+            if (e != null) {
+                Log.i(TAG, "onEvent: " + e.getLocalizedMessage());
+                return;
+            }
+            if (queryDocumentSnapshots != null) {
+                for (DocumentChange documentChange : queryDocumentSnapshots.getDocumentChanges()) {
+
+                    if (documentChange != null) {
+                        switch (documentChange.getType()) {
+                            case ADDED:
+                                Event addedEvent = documentChange.getDocument().toObject(Event.class);
+                                if (!mainFeedEvents.containsKey(addedEvent.getId())) {
+                                    mainFeedEvents.put(addedEvent.getId(), addedEvent);
+                                    Log.i(TAG, "onEvent: ADDED " + documentChange.getDocument().toObject(Event.class).toString());
+
+                                } else {
+                                    Log.i(TAG, "onEvent: NOT ADDED " + documentChange.getDocument().toObject(Event.class).toString());
+                                }
+                                eventFeedViewAdapter.notifyDataSetChanged();
+                                break;
+                            case MODIFIED:
+                                Log.i(TAG, "onEvent: MODIFIED " + documentChange.getDocument().toObject(Event.class).toString());
+                                break;
+                            case REMOVED:
+                                Log.i(TAG, "onEvent: REMOVED " + documentChange.getDocument().toObject(Event.class).toString());
+                                break;
+
+                        }
+                    }
+                }
+            }
+
+        }
+    };
 
 
     private final OnCompleteListener<DocumentSnapshot> playerCompleteListener = new OnCompleteListener<DocumentSnapshot>() {
@@ -197,14 +185,48 @@ public class FeedFragment extends Fragment {
                         });
                     }
                 }
+
+                setupInitalRecyclerView(false);
+                progressBar.setVisibility(View.GONE);
             }
         }
     };
 
-    Map<String, Event> filteredEventsByCategory = new HashMap<>();
-    List<String> selectedSports = new ArrayList<>();
 
-    private static final String TAG = "FeedFragment";
+    private void setupInitalRecyclerView(boolean initalSetup) {
+
+        if (initalSetup) {
+            if (popupMenu.getMenu().getItem(0).isChecked()) {
+                //filteredEventsByCategory = mDb.filterEventByFavoriteSport(mainFeedEvents, menuSports.get(0).getCategory(), menuSports.get(1).getCategory(), menuSports.get(2).getCategory());
+                eventFeedViewAdapter = new EventFeedViewAdapter(mainFeedEvents, getActivity().getApplicationContext());
+
+                eventsRecyclerView = getView().findViewById(R.id.rv_ongoing);
+                eventsRecyclerView.setHasFixedSize(true);
+                eventsRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
+
+                eventsRecyclerView.setAdapter(eventFeedViewAdapter);
+                eventsRecyclerView.setVisibility(View.GONE);
+            }
+        } else {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if (popupMenu.getMenu().getItem(0).isChecked()) {
+                        final String s1 = menuSports.get(0).getCategory();
+                        final String s2 = menuSports.get(1).getCategory();
+                        //final String s3 = menuSports.get(2).getCategory();
+                        filteredEventsByCategory = mDb.filterEventByFavoriteSport(mainFeedEvents, s1,s2);
+                        eventFeedViewAdapter = new EventFeedViewAdapter(filteredEventsByCategory, getActivity().getApplicationContext());
+
+                        eventsRecyclerView.setAdapter(eventFeedViewAdapter);
+                        eventsRecyclerView.setVisibility(View.VISIBLE);
+                    }
+                }
+            }, 350);
+        }
+
+
+    }
     private final View.OnClickListener filterListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
